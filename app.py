87b81c812031e6852a862cd431da42dcecbfc7f3
etchoum9519 <157910011+etchoum9519@users.noreply.github.com>
2026-08-25 13:54:11 +0200

    Update app.py

--- app.py
+++ app.py
@@ -3,9 +3,9 @@ from concurrent.futures import ThreadPoolExecutor
 from flask import Flask, request, jsonify
 from openpyxl import load_workbook
 
-from selenium import webdriver
-from selenium.webdriver.common.by import By
-from selenium.webdriver.common.keys import Keys
+#from selenium import webdriver
+#from selenium.webdriver.common.by import By
+#from selenium.webdriver.common.keys import Keys
 
 from decimal import Decimal
 import pandas as pd
